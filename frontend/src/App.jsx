08cc95ec3f1f12123b5460e3d3e04bfc9f5bc465
sachin yadav <yadavsachin3166@gmail.com>
2026-08-13 10:51:14 +0530

    updated projects

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -12,36 +12,28 @@ import UsersRoles from "./pages/admin/users and roles/UsersAndRoles";
 import Customers from "./pages/admin/customers-page/Customers";
 import AdminAccounts from "./pages/admin/account-page/Accounts";
 import Transactions from "./pages/admin/Transaction-page/Transactions";
-<<<<<<< HEAD
 import Reports from "./pages/admin/reports-page/Reports";
-=======
 
 // Manager
 import ManagerDashboard from "./pages/manager/ManagerDashboard";
 import ManagerCustomers from "./pages/manager/Customers";
 import Accounts from "./pages/manager/Accounts";
->>>>>>> origin/master
+import ManagerTransactions from "./pages/manager/ManagerTransactions";
 
 // Teller
 import TellerDashboard from "./pages/teller/TellerDashboard";
 import Deposit from "./pages/teller/Deposit";
 import Withdraw from "./pages/teller/Withdraw";
 import TransactionHistory from "./pages/teller/TransactionHistory";
-import ManagerTransactions from "./pages/manager/ManagerTransactions";
 
 export default function App() {
   return (
     <Routes>
       {/* Login */}
       <Route path="/" element={<Login />} />
-<<<<<<< HEAD
-      {/* Admin */}
-      <Route //now route is parent of admin-pages
-=======
 
       {/* ================= ADMIN ================= */}
       <Route
->>>>>>> origin/master
         path="/admin"
         element={
           <ProtectedRoutes>
@@ -56,88 +48,10 @@ export default function App() {
         <Route path="customers" element={<Customers />} />
         <Route path="accounts" element={<AdminAccounts />} />
         <Route path="transaction" element={<Transactions />} />
+        <Route path="reports" element={<Reports />} />
       </Route>
 
-<<<<<<< HEAD
-        {/* Branch */}
-
-        <Route
-          path="branches"
-          element={
-            <ProtectedRoutes>
-              <Branch />
-            </ProtectedRoutes>
-          }
-        />
-
-        <Route
-          path="branch/:id"
-          element={
-            <ProtectedRoutes>
-              <BranchDetails />
-            </ProtectedRoutes>
-          }
-        />
-        {/* //users-page */}
-        <Route
-          path="users-roles"
-          element={
-            <ProtectedRoutes>
-              <UsersRoles />
-            </ProtectedRoutes>
-          }
-        />
-
-        {/* cutomers-page */}
-        <Route
-          path="customers"
-          element={
-            <ProtectedRoutes>
-              <Customers />
-            </ProtectedRoutes>
-          }
-        />
-
-        {/* Account-Page */}
-        <Route
-          path="accounts"
-          element={
-            <ProtectedRoutes>
-              <AdminAccounts />
-            </ProtectedRoutes>
-          }
-        />
-        {/* Account-Page */}
-
-        {/* Transaction - Page */}
-        <Route
-          path="transaction"
-          element={
-            <ProtectedRoutes>
-              <Transactions />
-            </ProtectedRoutes>
-          }
-        />
-        {/* Transaction - Page */}
-        {/* Reports - Page */}
-        <Route
-          path="reports"
-          element={
-            <ProtectedRoutes>
-              <Reports />
-            </ProtectedRoutes>
-          }
-        />{/* Reports - Page */}
-        {/* Reports - Page - END */}
-
-
-      </Route>{" "}
-      {/* END-OF-ADMIN */}
-      {/* END-OF-ADMIN */}
-      {/* Branch Manager */}
-=======
       {/* ================= MANAGER ================= */}
->>>>>>> origin/master
       <Route
         path="/manager/:branchcode"
         element={
@@ -151,12 +65,7 @@ export default function App() {
         path="/manager/:branchcode/customers"
         element={
           <ProtectedRoutes>
-<<<<<<< HEAD
-            <ManagerCustomers />{" "}
-            {/* this page for where manager can add customers */}
-=======
             <ManagerCustomers />
->>>>>>> origin/master
           </ProtectedRoutes>
         }
       />
@@ -169,9 +78,7 @@ export default function App() {
           </ProtectedRoutes>
         }
       />
-<<<<<<< HEAD
-      {/* TellerDashboard */}
-=======
+
       <Route
         path="/manager/:branchcode/transactions"
         element={
@@ -182,7 +89,6 @@ export default function App() {
       />
 
       {/* ================= TELLER ================= */}
->>>>>>> origin/master
       <Route
         path="/teller/:branchcode"
         element={
@@ -191,12 +97,6 @@ export default function App() {
           </ProtectedRoutes>
         }
       />
-<<<<<<< HEAD
-      {/* //TransactionHistory */}
-      {/* <Route path="/teller/:branchcode" element={<TellerDashboard />} /> */}
-      <Route path="/teller/:branchcode/deposit" element={<Deposit />} />
-      <Route path="/teller/:branchcode/withdraw" element={<Withdraw />} />
-=======
 
       <Route
         path="/teller/:branchcode/deposit"
@@ -216,7 +116,6 @@ export default function App() {
         }
       />
 
->>>>>>> origin/master
       <Route
         path="/teller/:branchcode/history"
         element={
