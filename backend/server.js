2a76684f23b6bc9f6f228554e58aad2428aa4195
Prince gond <princegondrw123@gmail.com>
2026-08-15 16:10:10 +0530

    port number error solve

--- backend/server.js
+++ backend/server.js
@@ -58,7 +58,7 @@ const startServer = async () => {
 
     app.use(
       cors({
-        origin: "http://localhost:5173",
+        origin: ['http://localhost:5173', 'http://localhost:5174','http://localhost:5175'],
         credentials: true,
         methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
         allowedHeaders: ["Content-Type", "Authorization"],
